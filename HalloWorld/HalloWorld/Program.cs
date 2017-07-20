b8837da49f1ff73fc2cf74d26939cd0a976ed281
Carlos Henrique Reis <carlos_henreis@unifei.edu.br>
2017-07-19 21:50:44 -0300

    Meu primeiro Projeto (HalloWord)

---
+++ HalloWorld/HalloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo\n");
+            Console.ReadKey();
+        }
+    }
+}
